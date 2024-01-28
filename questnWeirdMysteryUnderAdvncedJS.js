f4707fffba0e4a01087314999ed60420676acde3
ShrutiDeore76 <shrutideore76@gmail.com>
2024-01-28 19:26:11 +0530

    Adding file for Task : The wierd mystery advanced Js

---
+++ questnWeirdMysteryUnderAdvncedJS.js
@@ -0,0 +1,65 @@
+class Student{
+    constructor(name,age,marks){
+      this.name=name;
+      this.age=age;
+      this.marks=marks;
+    }
+    setPlacementAge(minPlacementAge) {
+       //return a function which takes in argument -> minMarks
+       //returns true if students marks are gretaer than minMarks and age gretaer than minPlacementAge
+       //Complete this function only. Do not alter any other thing.
+
+        //by arrow function
+        return (minMarks) => {
+        if(this.marks>=minMarks && this.age>=minPlacementAge)
+          return true;
+        else
+          return false;
+
+          /*Also correct way
+          let tempMarks=this.marks;
+          let tempAge=this.age;
+          return function(minMarks){
+            if(tempMarks>=minMarks && tempAge>=minPlacementAge)
+              return true;
+            else
+              return false;
+              */
+       }
+    }
+  }
+  
+  
+  
+  //Do not touching anything below this line
+  
+  function createNewStudents(name, age, marks){
+    const Riya=new Student(name, age, marks);
+    
+    console.log(Riya.setPlacementAge(18)(40))
+  
+  }
+  
+  async function readInput() {
+  
+      let inputString = '';
+  
+      var output=[];
+  
+      process.stdin.on('data', inputStdin => {
+  
+        inputString += inputStdin;
+  
+        const inputArr = inputString.split(/(?:\r\n|\r|\n)/g)
+  
+        const argumentsArr = inputArr[0].split(',');
+  
+        createNewStudents(argumentsArr[0], Number(argumentsArr[1]), Number(argumentsArr[2]))
+  
+        process.exit();
+  
+      })
+  
+  }
+  
+  readInput();
