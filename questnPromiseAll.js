1a8ff5c8b3d84cda7f42e019f37f92c136e2b3d3
ShrutiDeore76 <shrutideore76@gmail.com>
2024-02-04 23:46:41 +0530

    Adding file for questn Promises.all

---
+++ questnPromiseAll.js
@@ -0,0 +1,34 @@
+const posts = [
+    { title: 'Post One', lastActivityTime: new Date().getTime() }
+  ];
+  
+  function updateLastUserActivityTime() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const updatedLastActivityTime = new Date().getTime();
+        posts.lastActivityTime = updatedLastActivityTime;
+        resolve(posts.lastActivityTime);
+      }, 1000);
+    });
+  }
+  
+  function createPost(post) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        post.lastActivityTime = new Date().getTime();
+        posts.push(post);
+        resolve(posts);
+      }, 1000);
+    });
+  }
+  
+  Promise.all([createPost({ title: "Post two" }), updateLastUserActivityTime()])
+    .then(([createPostResolved, updateActivityResolved]) => {
+      console.log("User Post : ", createPostResolved);
+      console.log("Last Activity Time : ", updateActivityResolved);
+    })
+    .catch(err => console.log(err));
+
+ 
+  
+  
